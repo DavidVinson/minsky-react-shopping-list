15caef87cca85a81c7a48a69d2b821cf8cfcb5de
David Vinson <davidvinson018@gmail.com>
2024-05-23 16:26:17 -0500

    sql file statements

--- database.sql
+++ database.sql
@@ -1,2 +1,34 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+-- Don't forget to add your create table SQL 
+-- It is also helpful to include some test data
+
+-- Create a database called "fs-react-shopping"
+
+DROP TABLE IF EXISTS "grocery";
+
+CREATE TABLE grocery (
+	id SERIAL PRIMARY KEY,
+	name varchar(80),
+	quantity numeric,
+	unit varchar(20)
+);
+
+-- POST grocery item
+INSERT INTO "grocery" ("name", "quantity", "unit") 
+VALUES ('apples', '1', 'lb'), ('oranges', '2', 'lb'), ('milt', '1', 'gal');
+
+-- GET grocery
+SELECT * FROM "grocery";
+
+-- UPDATE
+UPDATE "grocery" SET "name"='braeburn apples' WHERE "id"='1';
+
+-- DELETE
+DELETE FROM "grocery" WHERE "id"='1';
+
+-- POST another grocery item
+INSERT INTO "grocery" ("name", "quantity", "unit") 
+VALUES ('granny smith apples', '1', 'lb');
+
